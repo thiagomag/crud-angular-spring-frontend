d6ba98092dd807fdbebb7f6f6441077834271f4d
thiagomag <thiagomagdalena@gmail.com>
2022-01-17 23:24:23 -0300

    some commit

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -13,7 +13,7 @@ import { ErrorDialogComponent } from '../../shared/components/error-dialog/error
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category']
+  displayedColumns = ['_id', 'name', 'category']
 
   // coursesService: CoursesService;
 
